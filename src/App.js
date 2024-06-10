43a1335b55fd7f7b3526f848ff6e4d05c938ed87
Aebrahm Ramos <47078725+AebrahmRamos@users.noreply.github.com>
2024-06-10 17:38:38 +0800

    May site na!

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div class="text-center">
-        <h1 class="text-4xl font-bold mb-4">Coming Soon</h1>
-        <p class="text-gray-500">Donate for development costs!</p>
-        <img src="gcash-qr.jpg" alt="GCash QR Code" style="max-width: 25%; max-height: auto; margin: 0 auto;  margin-top: 5%;"/>
-      </div>
+      <header className="App-header">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold mb-4">Coming Soon</h1>
+          <p className="text-gray-500">Donate for development costs!</p>
+          <img src="gcash-qr.jpg" alt="GCash QR Code" style={{maxWidth: '25%', maxHeight: 'auto', margin: '0 auto'}}/>
+        </div>
+      </header>
     </div>
   );
 }
